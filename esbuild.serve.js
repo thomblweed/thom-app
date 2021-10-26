3339f5d449a76702bb121c78144ab43cb4448c94
Thomas Newman <thomblweed@gmail.com>
2021-10-26 19:39:21 +0100

    simple build and server esbuild scripts

--- esbuild.serve.js
+++ esbuild.serve.js
@@ -1,26 +1,25 @@
-const esbuild = require('esbuild');
-const { sassPlugin } = require('esbuild-sass-plugin');
+import { serve } from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
 
-esbuild
-  .serve(
-    {
-      servedir: 'www',
-      port: 1234
-    },
-    {
-      entryPoints: ['src/index.tsx'],
-      outdir: 'www/js',
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      define: { 'process.env.NODE_ENV': '"development"' },
-      plugins: [sassPlugin()]
-    }
-  )
+serve(
+  {
+    servedir: 'www',
+    port: 1234
+  },
+  {
+    entryPoints: ['src/index.tsx'],
+    outdir: 'www/js',
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    define: { 'process.env.NODE_ENV': '"development"' },
+    plugins: [sassPlugin()]
+  }
+)
   .then((server) => {
     console.info('>>> dev server running on port:', server.port);
   })
   .catch((error) => {
-    console.error(`error`, error);
+    console.error(`dev serve error ::>>`, error);
     process.exit(1);
   });
